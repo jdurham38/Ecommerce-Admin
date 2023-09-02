035e86e37289d643af9f71d2b8a41f9888ea09f8
Joshua Durham <jdurham9725@eagle.fgcu.edu>
2023-09-02 11:58:24 -0400

    testing

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -1,6 +1,5 @@
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
-
 import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
 
@@ -16,7 +15,7 @@ export async function OPTIONS() {
 
 export async function POST(
   req: Request,
-  { params }: { params: { storeId?: string } }
+  { params }: { params: { storeId: string } }
 ) {
   const { productIds } = await req.json();
 
@@ -27,9 +26,9 @@ export async function POST(
   const products = await prismadb.product.findMany({
     where: {
       id: {
-        in: productIds
-      }
-    }
+        in: productIds,
+      },
+    },
   });
 
   const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
@@ -38,54 +37,56 @@ export async function POST(
     line_items.push({
       quantity: 1,
       price_data: {
-        currency: 'USD',
+        currency: "USD",
         product_data: {
           name: product.name,
         },
-        unit_amount: product.price.toNumber() * 100
-      }
+        unit_amount: product.price.toNumber() * 100,
+      },
     });
   });
 
   const order = await prismadb.order.create({
     data: {
-      storeId: params.storeId || "", // Set a default value when storeId is undefined
+      storeId: params.storeId,
       isPaid: false,
       orderItems: {
         create: productIds.map((productId: string) => ({
           product: {
             connect: {
-              id: productId
-            }
-          }
-        }))
-      }
-    }
+              id: productId,
+            },
+          },
+        })),
+      },
+    },
   });
 
-  let successUrl = `${process.env.FRONTEND_STORE_URL}/cart?success=1`; // Default value
+  // Define your frontend store URLs based on the storeId
+  let successUrl = `${process.env.FRONTEND_STORE_URL}/cart?success=1`;
+  let cancelUrl = `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`;
 
-  if (params.storeId === 'website1') {
-    successUrl = `${process.env.FRONTEND_STORE_URL}/cart?success=1`;
-  } else if (params.storeId === 'website2') {
+  // Check storeId and update URLs accordingly
+  if (params.storeId === "store2") {
     successUrl = `${process.env.FRONTEND_STORE_URL_JACC}/cart?success=1`;
+    cancelUrl = `${process.env.FRONTEND_STORE_URL_JACC}/cart?canceled=1`;
   }
 
   const session = await stripe.checkout.sessions.create({
     line_items,
-    mode: 'payment',
-    billing_address_collection: 'required',
+    mode: "payment",
+    billing_address_collection: "required",
     phone_number_collection: {
       enabled: true,
     },
     success_url: successUrl,
-    cancel_url: `${successUrl}&canceled=1`,
+    cancel_url: cancelUrl,
     metadata: {
-      orderId: order.id
+      orderId: order.id,
     },
   });
 
   return NextResponse.json({ url: session.url }, {
-    headers: corsHeaders
+    headers: corsHeaders,
   });
-};
+}
