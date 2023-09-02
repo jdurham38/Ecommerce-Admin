1c2d4b87f2caaa6f2f39cebde29ac9a60ff223fb
Joshua Durham <jdurham9725@eagle.fgcu.edu>
2023-09-02 15:02:27 -0400

    test

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -15,69 +15,75 @@ export async function OPTIONS() {
 }
 
 export async function POST(
-  req: Request,
-  { params }: { params: { storeId: string } }
-) {
-  const { productIds } = await req.json();
-
-  if (!productIds || productIds.length === 0) {
-    return new NextResponse("Product ids are required", { status: 400 });
-  }
-
-  const products = await prismadb.product.findMany({
-    where: {
-      id: {
-        in: productIds
-      }
+    req: Request,
+    { params }: { params: { storeId: string } }
+  ) {
+    const { productIds } = await req.json();
+    const storeId = params.storeId;
+  
+    if (!productIds || productIds.length === 0) {
+      return new NextResponse("Product ids are required", { status: 400 });
     }
-  });
-
-  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
-
-  products.forEach((product) => {
-    line_items.push({
-      quantity: 1,
-      price_data: {
-        currency: 'USD',
-        product_data: {
-          name: product.name,
-        },
-        unit_amount: product.price.toNumber() * 100
+  
+    const products = await prismadb.product.findMany({
+      where: {
+        id: {
+          in: productIds
+        }
       }
     });
-  });
-
-  const order = await prismadb.order.create({
-    data: {
-      storeId: params.storeId,
-      isPaid: false,
-      orderItems: {
-        create: productIds.map((productId: string) => ({
-          product: {
-            connect: {
-              id: productId
+  
+    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+  
+    products.forEach((product) => {
+      line_items.push({
+        quantity: 1,
+        price_data: {
+          currency: 'USD',
+          product_data: {
+            name: product.name,
+          },
+          unit_amount: product.price.toNumber() * 100
+        }
+      });
+    });
+  
+    const order = await prismadb.order.create({
+      data: {
+        storeId: params.storeId,
+        isPaid: false,
+        orderItems: {
+          create: productIds.map((productId: string) => ({
+            product: {
+              connect: {
+                id: productId
+              }
             }
-          }
-        }))
+          }))
+        }
       }
-    }
-  });
-
-  const session = await stripe.checkout.sessions.create({
-    line_items,
-    mode: 'payment',
-    billing_address_collection: 'required',
-    phone_number_collection: {
-      enabled: true,
-    },
-    success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
-    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
-    metadata: {
-      orderId: order.id
-    },
-  });
-
-  return NextResponse.json({ url: session.url },  {
-    headers: corsHeaders
-  });
-};
+    });
+  
+    // Customize success and cancel URLs based on the storeId parameter
+    const successURL = `/store/${storeId}/cart?success=1`;
+    const cancelURL = `/store/${storeId}/cart?canceled=1`;
+  
+    const session = await stripe.checkout.sessions.create({
+      line_items,
+      mode: 'payment',
+      billing_address_collection: 'required',
+      phone_number_collection: {
+        enabled: true,
+      },
+      success_url: successURL,
+      cancel_url: cancelURL,
+      metadata: {
+        orderId: order.id
+      },
+    });
+  
+    return NextResponse.json({ url: session.url },  {
+      headers: corsHeaders
+    });
+  };
+  
