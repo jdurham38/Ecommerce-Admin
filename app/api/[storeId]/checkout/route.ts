33c1d805fb409e7af8e045b380503757fc9e6353
Joshua Durham <jdurham9725@eagle.fgcu.edu>
2023-09-02 14:04:57 -0400

    revert

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -77,23 +77,11 @@ export async function POST(
     },
   });
 
-  const session2 = await stripe.checkout.sessions.create({
-    line_items,
-    mode: 'payment',
-    billing_address_collection: 'required',
-    phone_number_collection: {
-      enabled: true,
-    },
-    success_url: `${process.env.FRONTEND_STORE_UR_JACCL}/cart?success=1`,
-    cancel_url: `${process.env.FRONTEND_STORE_URL_JACC}/cart?canceled=1`,
-    metadata: {
-      orderId: order.id
-    },
-  });
+
 
   
 
-  return NextResponse.json({ url: session.url || session2.url },  {
+  return NextResponse.json({ url: session.url },  {
     headers: corsHeaders
   });
 };
